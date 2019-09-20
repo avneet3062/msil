74c236067b33aa5ee2ff6a36538c1c8b748526a8
avneet3062 <avnibachu@gmail.com>
2019-09-20 18:31:59 +0530

    All files.

--- src/main/java/com/nxtLife/msil/service/DataService.java
+++ src/main/java/com/nxtLife/msil/service/DataService.java
@@ -2,10 +2,14 @@ package com.nxtLife.msil.service;
 
 import com.nxtLife.msil.repository.TripRepository;
 import com.nxtLife.msil.views.TripMetrics;
+import com.nxtLife.msil.views.TripTypes;
+import com.nxtLife.msil.views.Trips;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +19,29 @@ public class DataService {
     @Autowired
     private TripRepository tripRepository;
 
-    public List<TripMetrics> getTripsMetrics(int year, int month){
+    public TripMetrics getTripsMetrics(int year, int month){
         //TODO- check for year and month
+
+        TripMetrics metrics=new TripMetrics();
         try {
-            Long count_opentrips = tripRepository.getOpenTrips();
-            System.out.println(count_opentrips);
+            Long openTrips = tripRepository.getOpenTrips();
+            Long closedTrips= tripRepository.getClosedTrips();
+            Long delayedTrips= tripRepository.getDelayedTrips();
+            Long totalTrips= tripRepository.getTotalTrips();
+
+            List<Trips> tripsList = new ArrayList<>();
+            tripsList.add(new Trips(TripTypes.Open.name(),openTrips));
+            tripsList.add(new Trips(TripTypes.Closed.name(),closedTrips));
+            tripsList.add(new Trips(TripTypes.Delayed.name(),delayedTrips));
+            tripsList.add(new Trips(TripTypes.Total.name(),totalTrips));
+
+            metrics = new TripMetrics(year,month,tripsList,Date.valueOf(LocalDate.now().minusDays(13)));
+
+
         }catch (SQLException e){
             e.printStackTrace();
         }
 
-        return new ArrayList<>();
+        return metrics;
     }
 }
