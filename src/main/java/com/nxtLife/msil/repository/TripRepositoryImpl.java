da4b433c2f4f150509ae294ff4747ad325f11e6e
avneet3062 <avnibachu@gmail.com>
2019-09-20 18:33:18 +0530

    All files.

--- src/main/java/com/nxtLife/msil/repository/TripRepositoryImpl.java
+++ src/main/java/com/nxtLife/msil/repository/TripRepositoryImpl.java
@@ -8,9 +8,14 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.ParameterMode;
 import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public class TripRepositoryImpl implements TripRepository {
@@ -20,9 +25,39 @@ public class TripRepositoryImpl implements TripRepository {
 
     @Override
     public Long getOpenTrips() throws SQLException {
-        Long countOfOpenTrips=0l;
-//                (Long) em.unwrap(Session.class).createSQLQuery("{call MSIL_OPENTRIPS(?,?)}")
-//                .setParameter(1, ParameterMode.REF_CURSOR).setParameter(2,java.sql.Date.valueOf(LocalDate.now())).uniqueResult();
-        return countOfOpenTrips;
+        java.sql.Timestamp date= java.sql.Timestamp.valueOf(LocalDateTime.now().minusDays(13).withHour(0).withMinute(0).withSecond(0));
+        BigDecimal countOfOpenTrips= (BigDecimal) em.unwrap(Session.class).createStoredProcedureCall("MSIL_OPENTRIPS")
+                .registerStoredProcedureParameter(1, Class.class,ParameterMode.REF_CURSOR)
+                .registerStoredProcedureParameter(2, Timestamp.class, ParameterMode.IN).setParameter(2,date).getSingleResult();
+
+//        List<Object> obj= em.unwrap(Session.class).createStoredProcedureCall("ETRK_VEH_DIS")
+//                .registerStoredProcedureParameter(1,Class.class,ParameterMode.REF_CURSOR).getResultList();
+        return countOfOpenTrips.longValue();
+    }
+
+    @Override
+    public Long getClosedTrips() throws SQLException {
+        java.sql.Timestamp date= java.sql.Timestamp.valueOf(LocalDateTime.now().minusDays(13).withHour(0).withMinute(0).withSecond(0));
+        BigDecimal countOfClosedTrips= (BigDecimal) em.unwrap(Session.class).createStoredProcedureCall("MSIL_CLOSEDTRIPS")
+                .registerStoredProcedureParameter(1, Class.class,ParameterMode.REF_CURSOR)
+                .registerStoredProcedureParameter(2, Timestamp.class, ParameterMode.IN).setParameter(2,date).getSingleResult();
+        return countOfClosedTrips.longValue();
+    }
+
+    @Override
+    public Long getDelayedTrips() throws SQLException {
+        java.sql.Timestamp date= java.sql.Timestamp.valueOf(LocalDateTime.now().minusDays(13).withHour(0).withMinute(0).withSecond(0));
+        BigDecimal countOfDelayedTrips= (BigDecimal) em.unwrap(Session.class).createStoredProcedureCall("MSIL_DELAYTRIPS")
+                .registerStoredProcedureParameter(1, Class.class,ParameterMode.REF_CURSOR).getSingleResult();
+        return countOfDelayedTrips.longValue();
+    }
+
+    @Override
+    public Long getTotalTrips() throws SQLException {
+        java.sql.Timestamp date= java.sql.Timestamp.valueOf(LocalDateTime.now().minusDays(13).withHour(0).withMinute(0).withSecond(0));
+        BigDecimal countOfTotalTrips= (BigDecimal) em.unwrap(Session.class).createStoredProcedureCall("MSIL_TOTALTRIPS")
+                .registerStoredProcedureParameter(1, Class.class,ParameterMode.REF_CURSOR)
+                .registerStoredProcedureParameter(2, Timestamp.class, ParameterMode.IN).setParameter(2,date).getSingleResult();
+        return countOfTotalTrips.longValue();
     }
 }
