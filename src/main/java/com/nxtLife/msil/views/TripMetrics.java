50d53e9a7743b61d8f80d7a8a7a73246b15e8d62
avneet3062 <avnibachu@gmail.com>
2019-09-20 18:32:57 +0530

    All files.

--- src/main/java/com/nxtLife/msil/views/TripMetrics.java
+++ src/main/java/com/nxtLife/msil/views/TripMetrics.java
@@ -5,19 +5,25 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Setter
 @Getter
-@NoArgsConstructor
 public class TripMetrics {
     int year;
     int month;
+    Date date;
     List<Trips> tripsList = new ArrayList<>();
 
-    public TripMetrics(int year, int month, List<Trips> tripsList) {
+    public TripMetrics(int year, int month, List<Trips> tripsList,Date date) {
         this.year = year;
         this.month = month;
         this.tripsList = tripsList;
+        this.date = date;
+    }
+
+    public TripMetrics() {
+
     }
 }
